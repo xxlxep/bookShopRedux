03c9ad8348bb12f7c839fb84f19649ab365148fd
xxlxep <xxlxep@gmail.com>
2019-06-17 16:38:34 +0300

    Async data loading Added

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,16 +5,19 @@ import { withBookstoreService } from '../hoc';
 import { booksLoaded } from '../../actions';
 import { compose } from '../../utils';
 import './book-list.css';
+import Spinner from '../spinner';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-    this.props.booksLoaded(data);
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks().then(data => booksLoaded(data));
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+    if (loading) {
+      return <Spinner />;
+    }
     return (
       <ul className="book-list">
         {books.map(book => {
@@ -28,8 +31,8 @@ class BookList extends Component {
     );
   }
 }
-const mapStateToProps = ({ books }) => {
-  return { books };
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
 };
 
 const mapDispatchToProps = {
--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './shop-header.css';
+import { Link } from 'react-router-dom';
 
 const ShopHeader = ({ numItems, total }) => {
   return (
     <header className="shop-header row">
-      <a className="logo text-dark" href="#">
-        ReStore
-      </a>
-      <a className="shopping-cart">
-        <i className="cart-icon fa fa-shopping-cart" />
-        {numItems} items (${total})
-      </a>
+      <Link to="/">
+        <div className="logo text-dark" href="#">
+          ReStore
+        </div>
+      </Link>
+      <Link to="/">
+        <div className="shopping-cart" href="#">
+          <i className="cart-icon fa fa-shopping-cart" />
+          {numItems} items (${total})
+        </div>
+      </Link>
     </header>
   );
 };
